628cc416aaa89d351ff192367aea7c0dd3462300
akashpoojari1450-crypto <akashpoojari1450@gmail.com>
2026-08-03 01:31:01 +0000

    nested

---
+++ nested.py
@@ -0,0 +1,17 @@
+#using nested
+for i in range(6):
+    for j in range(i):
+        print(i,end=' ')
+print()
+ #output=1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 
+
+ #using nested
+for i in range(6):
+    for j in range(i):
+        print(i,end=' ')
+    print()
+#output=  1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
