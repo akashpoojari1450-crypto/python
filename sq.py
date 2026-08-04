206ca6b51ba40ec5d3833a56121e065e9290a2b4
akashpoojari1450-crypto <akashpoojari1450@gmail.com>
2026-08-04 01:49:52 +0000

    square

---
+++ sq.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number "))
+for i in range(1,n):
+    m=n**2
+    print("The sqauare is:",m)
+    break
+
+#using seperate
+n=int(input("Enter the number "))
+for i in range(1,n+1):
+    print("the square of",i,"is:",i**2)    
